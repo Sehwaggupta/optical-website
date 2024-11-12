b01f5a4be47dfff8e08a0adbfcc3142da02d104c
Sehwaggupta <sehwaggupta279@gmail.com>
2024-11-12 20:02:19 +0530

    optical website

---
+++ optical_website/script.js
@@ -0,0 +1,197 @@
+document.getElementsByClassName('col-doller')[0].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[0].src='../image/icons/bluedoller.png';
+    document.getElementsByClassName('doller-link')[0].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[0].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[0].src='../image/icons/dollericon.png';
+    document.getElementsByClassName('doller-link')[0].style.color = 'white';
+})
+
+
+
+
+document.getElementsByClassName('col-doller')[1].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[1].src='../image/icons/truck_blue.png';
+    document.getElementsByClassName('doller-link')[1].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[1].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[1].src='../image/icons/truck.png';
+    document.getElementsByClassName('doller-link')[1].style.color = 'white';
+})
+
+
+
+
+
+document.getElementsByClassName('col-doller')[2].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[2].src='../image/icons/glass_blue.png';
+    document.getElementsByClassName('doller-link')[2].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[2].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[2].src='../image/icons/glass.png';
+    document.getElementsByClassName('doller-link')[2].style.color = 'white';
+})
+
+
+
+
+
+
+
+document.getElementsByClassName('col-doller')[3].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[3].src='../image/icons/ruller_blue.png';
+    document.getElementsByClassName('doller-link')[3].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[3].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[3].src='../image/icons/ruller.png';
+    document.getElementsByClassName('doller-link')[3].style.color = 'white';
+})
+
+
+
+
+
+
+
+document.getElementsByClassName('col-doller')[4].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[4].src='../image/icons/load_blue.png';
+    document.getElementsByClassName('doller-link')[4].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[4].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[4].src='../image/icons/load.png';
+    document.getElementsByClassName('doller-link')[4].style.color = 'white';
+})
+
+
+
+
+
+document.getElementsByClassName('col-doller')[5].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[5].src='../image/icons/shield_blue.png';
+    document.getElementsByClassName('doller-link')[5].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[5].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[5].src='../image/icons/shield.png';
+    document.getElementsByClassName('doller-link')[5].style.color = 'white';
+})
+
+
+
+
+document.getElementsByClassName('col-doller')[6].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[6].src='../image/icons/i blue.png';
+    document.getElementsByClassName('doller-link')[6].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[6].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[6].src='../image/icons/1i.png';
+    document.getElementsByClassName('doller-link')[6].style.color = 'white';
+})
+
+
+
+
+document.getElementsByClassName('col-doller')[7].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[7].src='../image/icons/cemera blue.png';
+    document.getElementsByClassName('doller-link')[7].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[7].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[7].src='../image/icons/cemera.png';
+    document.getElementsByClassName('doller-link')[7].style.color = 'white';
+})
+
+
+
+
+document.getElementsByClassName('col-doller')[8].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[8].src='../image/icons/ruler blue.png';
+    document.getElementsByClassName('doller-link')[8].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[8].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[8].src='../image/icons/ruler.png';
+    document.getElementsByClassName('doller-link')[8].style.color = 'white';
+})
+
+
+
+
+
+
+document.getElementsByClassName('col-doller')[9].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[9].src='../image/icons/tag blue.png';
+    document.getElementsByClassName('doller-link')[9].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[9].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[9].src='../image/icons/tag.png';
+    document.getElementsByClassName('doller-link')[9].style.color = 'white';
+})
+
+
+
+
+
+document.getElementsByClassName('col-doller')[10].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[10].src='../image/icons/hand blue.png';
+    document.getElementsByClassName('doller-link')[10].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[10].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[10].src='../image/icons/hand.png';
+    document.getElementsByClassName('doller-link')[10].style.color = 'white';
+})
+
+
+
+
+
+document.getElementsByClassName('col-doller')[11].addEventListener('mouseover',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[11].src='../image/icons/id blue.png';
+    document.getElementsByClassName('doller-link')[11].style.color = '#72a2fc';
+})
+
+document.getElementsByClassName('col-doller')[11].addEventListener('mouseout',()=>{
+    // alert("h")
+    document.getElementsByClassName('doller')[11].src='../image/icons/id.png';
+    document.getElementsByClassName('doller-link')[11].style.color = 'white';
+})
+
+
+
+
+
+
+
+
+
